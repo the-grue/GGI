ad44666687e0a8e2a5044875ca89ebbed8948e0c
James T. Sprinkle <the-grue@hotmail.com>
2021-01-09 16:38:18 -0800

    Added thickness to circles.

--- src/circle.c
+++ src/circle.c
@@ -1,12 +1,51 @@
 #include <graphics.h>
 
 extern unsigned char currcolor;
+extern struct linesettingstype linesettingstype;
 
 void circle(int x, int y, int radius)
 {
 	int xm = -radius, ym = 0, err = 2 - 2 * radius;
 
 	do {
+		if(linesettingstype.thickness == THICK_WIDTH)
+		{
+			putpixel(x-xm-1, y+ym-1, currcolor);
+			putpixel(x-xm-1, y+ym, currcolor);
+			putpixel(x-xm-1, y+ym+1, currcolor);
+			putpixel(x-xm, y+ym-1, currcolor);
+			putpixel(x-xm, y+ym+1, currcolor);
+			putpixel(x-xm+1, y+ym+1, currcolor);
+			putpixel(x-xm+1, y+ym, currcolor);
+			putpixel(x-xm+1, y+ym-1, currcolor);
+
+			putpixel(x-xm-1, y-ym-1, currcolor);
+			putpixel(x-xm-1, y-ym, currcolor);
+			putpixel(x-xm-1, y-ym+1, currcolor);
+			putpixel(x-xm, y-ym-1, currcolor);
+			putpixel(x-xm, y-ym+1, currcolor);
+			putpixel(x-xm+1, y-ym+1, currcolor);
+			putpixel(x-xm+1, y-ym, currcolor);
+			putpixel(x-xm+1, y-ym-1, currcolor);
+
+			putpixel(x+xm-1, y-ym-1, currcolor);
+			putpixel(x+xm-1, y-ym, currcolor);
+			putpixel(x+xm-1, y-ym+1, currcolor);
+			putpixel(x+xm, y-ym-1, currcolor);
+			putpixel(x+xm, y-ym+1, currcolor);
+			putpixel(x+xm+1, y-ym+1, currcolor);
+			putpixel(x+xm+1, y-ym, currcolor);
+			putpixel(x+xm+1, y-ym-1, currcolor);
+
+			putpixel(x+xm-1, y+ym-1, currcolor);
+			putpixel(x+xm-1, y+ym, currcolor);
+			putpixel(x+xm-1, y+ym+1, currcolor);
+			putpixel(x+xm, y+ym-1, currcolor);
+			putpixel(x+xm, y+ym+1, currcolor);
+			putpixel(x+xm+1, y+ym+1, currcolor);
+			putpixel(x+xm+1, y+ym, currcolor);
+			putpixel(x+xm+1, y+ym-1, currcolor);
+		}
 		putpixel(x-xm, y+ym, currcolor);
 		putpixel(x-ym, y-xm, currcolor);
 		putpixel(x+xm, y-ym, currcolor);
